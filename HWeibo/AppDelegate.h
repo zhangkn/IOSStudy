55d99d7ab9ee87c070c720495845dd2ebfef5ad3
Kevin zhang <929118967@qq.com>
2016-06-13 10:59:08 +0800

    Initial Commit

---
+++ HWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWeibo
+//
+//  Created by devzkn on 6/13/16.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HWeibo
+//
+//  Created by devzkn on 6/13/16.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
