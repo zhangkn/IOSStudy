e771a11905f9c94a50b1e0dca75cbf5dfcaba48c
Kevin zhang <929118967@qq.com>
2016-07-20 11:38:29 +0800

    添加搜索框

--- HWeibo/HWDiscoverTableViewController.m
+++ HWeibo/HWDiscoverTableViewController.m
@@ -16,17 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 //    UISearchBar *searchBar = [[UISearchBar alloc]init];
     UITextField *searchBar = [[UITextField alloc]init];
     searchBar.width = 300;
     searchBar.height =30;
+    searchBar.font = [UIFont systemFontOfSize:12];
+    searchBar.placeholder = @"大家正在搜：霉霉侃爷撕逼";
     searchBar.background = [UIImage imageNamed:@"searchbar_textfield_background"];
+    //设置文本输入框的左视图
+    UIImageView *searchIcon = [[UIImageView alloc]init];
+    searchIcon.image = [UIImage imageNamed:@"searchbar_textfield_search_icon"];
+    searchIcon.width =30;
+    searchIcon.height =30;
+    [searchIcon setContentMode:UIViewContentModeCenter];
+    searchBar.leftView= searchIcon;
+    [searchBar setLeftViewMode:UITextFieldViewModeUnlessEditing];
+
     self.navigationItem.titleView =searchBar;
 }
 
