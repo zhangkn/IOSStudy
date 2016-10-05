a36efe978d64e44f8b79ae640249aed7d45110df
kevin <929118967@qq.com>
2016-10-05 13:44:39 +0800

    //git 常用命令： //devzkndeMacBook-Pro:HWeibo devzkn$ git reflog //其中—hard代表强制重置 //git reset —hard 版本号（只需要写前7位）

--- HWeibo/AppDelegate.h
+++ HWeibo/AppDelegate.h
@@ -1,7 +1,10 @@
 //
 //  AppDelegate.h
 //  HWeibo
-//
+//git 常用命令：
+//devzkndeMacBook-Pro:HWeibo devzkn$ git reflog
+//其中—hard代表强制重置
+//git reset —hard 版本号（只需要写前7位）
 //  Created by devzkn on 6/13/16.
 //  Copyright © 2016 hisun. All rights reserved.
 //
